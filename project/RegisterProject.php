089e8e0e7cadeb73c093803f80c6d3151f1a4ba4
MingXian02 <93081875+MingXian02@users.noreply.github.com>
2023-06-15 01:51:54 +0800

    Update RegisterProject.php

--- project/RegisterProject.php
+++ project/RegisterProject.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register your project</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style/RegisterProject.css">
 </head>
 
 <body>
@@ -14,7 +16,7 @@
     include 'PDO.php';
     if (!isset($_GET['username'])) {
         echo "<h1>You have not logged in</h1><br/>";
-        echo "<a href='login.php'>Please Login</a><br/><br/>";
+        echo "<a href='login.php'>aPlease Login</a><br/><br/>";
         die("Name parameter missing");
     }
     ?>
