20e0a16ad9f9e20c1de8d3e7f2558d26ffb1dfa3
MingXian02 <93081875+MingXian02@users.noreply.github.com>
2023-06-15 01:51:01 +0800

    Update ProjectList.php

--- project/ProjectList.php
+++ project/ProjectList.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>THEN NAN HUI 213054</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style/ProjectList.css">
 </head>
 
 <body>
@@ -64,6 +66,7 @@
     ?>
     <br>
     <a href="RegisterProject.php?username=<?php echo $url ?>">Register Project</a>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
