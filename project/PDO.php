a89bf27c25ca98c3692e28bb19ff679707440b6a
benthen <111986781+benthen@users.noreply.github.com>
2023-06-18 00:35:30 +0800

    Delete project directory

--- project/PDO.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $pdo = new PDO(
-        'mysql:host=localhost;port=3306;dbname=web',
-        'root',
-        '1234'
-    );
-    // See the "errors" folder for details...
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    ?>
